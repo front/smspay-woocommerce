e80ce3e7ab85ae5f4b46231d764267d13419d42a
Roberto Ornelas <rob@front.no>
2015-06-22 15:33:05 +0100

    Readme.txt (wip)

--- SMSpay.php
+++ SMSpay.php
@@ -14,8 +14,8 @@
  * Version:     1.0.0
  * Author:      NextLogic / Frontkom
  * Author URI:  https://www.frontkom.no/
- * License:     GPL-2.0+
- * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ * License:     GPLv2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: smspay
  * Domain Path: /languages
  */
